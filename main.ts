e1029da099ec3605203581993b37f1a5638d8bca
tjy163 <112043389+tomistjy@users.noreply.github.com>
2025-01-23 18:10:59 +0800

    屏幕显示与小车运动控制

--- main.ts
+++ main.ts
@@ -1,19 +1,66 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        basic.showNumber(1)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Run, 200)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
     } else if (receivedNumber == 2) {
-        basic.showNumber(2)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Back, 200)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     } else if (receivedNumber == 3) {
-        basic.showNumber(3)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Left, 175)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (receivedNumber == 4) {
-        basic.showNumber(4)
+        cbit_小车类.CarCtrlSpeed(cbit_小车类.CarState.Car_Right, 175)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     } else if (receivedNumber == 0) {
-        basic.showNumber(0)
+        cbit_小车类.CarCtrl(cbit_小车类.CarState.Car_Stop)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     } else {
-        basic.showNumber(0)
+        cbit_小车类.CarCtrl(cbit_小车类.CarState.Car_Stop)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
     }
 })
 radio.setGroup(1)
 cbit_小车类.RGB_Car_Big(170, 170, 170)
 cbit_小车类.RGB_Car_Program().show()
 cbit_小车类.RGB_Car_Program().showColor(neopixel.colors(NeoPixelColors.White))
+loops.everyInterval(50, function () {
+    if (cbit_小车类.Ultrasonic_Car() < 10) {
+        cbit_小车类.CarCtrl(cbit_小车类.CarState.Car_Stop)
+    }
+})
